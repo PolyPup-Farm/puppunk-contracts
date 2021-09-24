44a4822c907a8cebe92a0655baaf5032fc374c7a
Polypup <85349906+Polypup@users.noreply.github.com>
2021-09-24 15:02:09 -0400

    Fixes

--- PupPunks.sol
+++ PupPunks.sol
@@ -13,7 +13,6 @@
 pragma solidity ^0.8.4;
 
 import '@openzeppelin/contracts/utils/Counters.sol';
-import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol';
@@ -21,9 +20,9 @@ import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 import '@openzeppelin/contracts/utils/Strings.sol';
-
+import '@openzeppelin/contracts/utils/Address.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
-import 'hardhat/console.sol';
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
 
 contract PolyPupPunks is
 	ERC721Pausable,
@@ -33,14 +32,13 @@ contract PolyPupPunks is
 	ReentrancyGuard
 {
 	using Counters for Counters.Counter;
-	using SafeMath for uint256;
 
 	bool private sale;
 
 	uint256 public constant TOKEN_LIMIT = 100;
 	uint256 public constant PRICE = 75 ether; // 0.06 ETH
 	uint256 public constant MAX_MINT_AT_ONCE = 10;
-	address payable internal developer;
+	address payable internal immutable developer;
 	string public baseTokenURI;
 
 	Counters.Counter private _numberOfTokens;
@@ -48,9 +46,9 @@ contract PolyPupPunks is
 	// Random index assignment
 	uint256 internal nonce = 0;
 	uint256[TOKEN_LIMIT] internal indices;
+	address public immutable pairAddress =
+		0x853Ee4b2A13f8a742d64C8F088bE7bA2131f670d; // USDC/WETH QuickSwap Pair Address
 
-	mapping(address => uint256[]) internal ownerToIds;
-	mapping(uint256 => uint256) internal idToOwnerIndex;
 	/**
 	 * Event emitted when poly pup punk minted
 	 */
@@ -61,6 +59,11 @@ contract PolyPupPunks is
 	 * Event emitted when the public sale begins.
 	 */
 	event SaleBegins();
+
+	/**
+	 * Event emitted when base uri is updated
+	 */
+	event BaseURIUpdated();
 
 	constructor(string memory baseURI, address payable _developer)
 		ERC721('PolyPup Punks', 'POLYPUP')
@@ -77,6 +80,7 @@ contract PolyPupPunks is
 	function startSale() public onlyOwner {
 		sale = true;
 		pause(false);
+		emit SaleBegins();
 	}
 
 	/**
@@ -99,18 +103,18 @@ contract PolyPupPunks is
 	 * Public method to start minting PolyPup Punks
 	 */
 
-	function mint(address _to, uint256 _count) external payable nonReentrant {
+	function mint(uint256 _count) external payable nonReentrant {
 		uint256 total = _totalSupply();
 		require(sale == true, 'Sale has not yet started');
-		require(total <= TOKEN_LIMIT, 'Sale ended');
 		require(total + _count <= TOKEN_LIMIT, 'Max limit');
 		require(_count <= MAX_MINT_AT_ONCE, 'Exceeds number');
-		require(msg.value >= price(_count), 'Value below price');
-		// TODO: If this is the safest method to transfer
-		payable(developer).transfer(msg.value);
+		require(msg.value == price(_count), 'Value below price');
+		require(tx.origin == msg.sender, 'Caller cannot be a contract'); // Only EOA can mint and not a contract
+
+		Address.sendValue(developer, msg.value);
 
 		for (uint256 i = 0; i < _count; i++) {
-			_mintAnElement(_to);
+			_mintAnElement(msg.sender);
 		}
 	}
 
@@ -127,9 +131,18 @@ contract PolyPupPunks is
 	 */
 	function _randomIndex() internal returns (uint256) {
 		uint256 totalSize = TOKEN_LIMIT - _numberOfTokens.current();
+		//Fetching current balances of USDC/WETH QS Pair for increasing Randomness Entropy
+		(uint256 balance1, uint256 balance2) = getCurrentBalanceQSPair();
 		uint256 index = uint256(
 			keccak256(
-				abi.encodePacked(nonce, msg.sender, block.difficulty, block.timestamp)
+				abi.encodePacked(
+					nonce,
+					msg.sender,
+					balance1,
+					balance2,
+					block.difficulty,
+					block.timestamp
+				)
 			)
 		) % totalSize;
 		uint256 value = 0;
@@ -149,7 +162,18 @@ contract PolyPupPunks is
 		}
 		nonce++;
 		// Don't allow a zero index, start counting at 1
-		return value.add(1);
+		return value + 1;
+	}
+
+	/**
+	 * @dev Getting current reserves of USDC and WETH pairs on QuickSwap
+	 */
+	function getCurrentBalanceQSPair() public view returns (uint256, uint256) {
+		IUniswapV2Pair pair = IUniswapV2Pair(
+			0x853Ee4b2A13f8a742d64C8F088bE7bA2131f670d // Address of USDC/WETH Pair on QuickSwap
+		);
+		(uint256 res0, uint256 res1, ) = pair.getReserves();
+		return (res0, res1);
 	}
 
 	/** Returns total minted at that point of time */
@@ -159,7 +183,7 @@ contract PolyPupPunks is
 
 	/** Returns total price for total numbrer of minting */
 	function price(uint256 _count) public pure returns (uint256) {
-		return PRICE.mul(_count);
+		return PRICE * _count;
 	}
 
 	/** Defining Internal Methods */
@@ -176,6 +200,7 @@ contract PolyPupPunks is
 	 */
 	function setBaseURI(string memory baseURI) public onlyOwner {
 		baseTokenURI = baseURI;
+		emit BaseURIUpdated();
 	}
 
 	/**
@@ -191,21 +216,16 @@ contract PolyPupPunks is
 		validNFToken(_tokenId)
 		returns (string memory)
 	{
-		return
-			string(
-				abi.encodePacked(
-					baseTokenURI,
-					'api/polypup-punks/',
-					Strings.toString(_tokenId)
-				)
-			);
+		// returning only baseTokenURI followed by the ID.
+		return string(abi.encodePacked(baseTokenURI, Strings.toString(_tokenId)));
 	}
 
 	/**
 	 * Pause and unpause sale
 	 */
 	function pause(bool val) public onlyOwner {
-		if (val == true) {
+		// Allowing pause only if the sale hasn't started
+		if (val == true && sale == false) {
 			_pause();
 			return;
 		}
@@ -227,18 +247,6 @@ contract PolyPupPunks is
 
 		return tokenIds;
 	}
-
-	// function withdrawAll() public payable onlyOwner {
-	// 	uint256 balance = address(this).balance;
-	// 	require(balance > 0);
-	// 	_widthdraw(developer, balance.mul(60).div(100));
-	// 	_widthdraw(official, address(this).balance);
-	// }
-
-	// function _widthdraw(address _address, uint256 _amount) private {
-	// 	(bool success, ) = _address.call{value: _amount}('');
-	// 	require(success, 'Transfer failed.');
-	// }
 
 	function _beforeTokenTransfer(
 		address from,
